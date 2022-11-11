bb448cbd6429395e9fbfd2d686a094d637d7e5fa
xvovabluex <95418222+xvovabluex@users.noreply.github.com>
2022-11-11 21:39:46 +0300

    Add files via upload

---
+++ data/hashes/install.bat
@@ -0,0 +1,34 @@
+@echo off
+echo Welcome to hash installer!
+pause
+echo You Want to install hash?
+pause
+echo Verify in folder has empty
+pause
+::mysubsarethebest
+echo Win32.Trojan.MrsMajor Dropper >> 247a35851fdee53a1696715d67bd0905
+echo Win32.Trojan.MrsMajor2.0 >> 91a0740cfb043e1f4d8461f8cbe2ff19
+echo Win32.Trojan.MrsMajor2.0 >> 87a43b15969dc083a0d7e2ef73ee4dd1
+echo Win32.Trojan.MrsMajor2.0 >> 1aaafedd9f259acca75708f4af10b5be
+echo Win32.Trojan.MrsMajor2.0 >> b561c360c46744f55be79a25e1844e3c
+echo Win32.Trojan.MrsMajor2.0 >> 230970ec5286b34a6b2cda9afdd28368
+echo Win32.Trojan.MrsMajor2.0 >> 570d35aabee1887f7f6ab3f0a1e76984
+echo Win32.Trojan.MrsMajor2.0 >> d604c29940864c64b4752d31e2deb465
+echo Win32.Trojan.MrsMajor2.0 >> 57f3795953dafa8b5e2b24ba5bfad87f
+echo Win32.Trojan.MrsMajor2.0 >> bcb0ac4822de8aeb86ea8a83cd74d7ca
+echo Win32.Trojan.Memz >> 4e2a7f369378a76d1df4d8c448f712af
+echo Win32.Trojan.Memz >> 19dbec50735b5f2a72d4199c4e184960
+echo Win32.Trojan.MrsMajor3.0 >> 35a27d088cd5be278629fae37d464182
+echo Win32.Trojan.NoEscape >> 989ae3d195203b323aa2b3adf04e9833
+echo Win32.MalwareNotWaring.000 >> f2b7074e1543720a9a98fda660e02688
+echo Win32.Ransomware.7ev3n >> 9f8bc96c96d43ecb69f883388d228754
+echo Win32.Ransomware.WinlockerVB6Blacksod >>dbfbf254cfb84d991ac3860105d66fc5
+echo Win32.Trojan.Ana >> f571faca510bffe809c76c1828d44523
+echo Win32.Ransomware.PetyaA >> af2379cc4d607a45ac44d62135fb7015
+echo Win32.Ransomware.WannaCrypt0r >> 84c82835a5d21bbcf75a61706d8ab549
+echo Win32.Ransomware.BadRabbit >> fbbdc39af1139aebba4da004475e8839
+echo Win32.Trojan.TaskILL >> c261c6e3332d0d515c910bbf3b93aab3
+echo Win32.Trojan.Illerka.C >> c718a1cbf0e13674714c66694be02421
+echo Win32.Trojan.HMBlocker >> 21943d72b0f4c2b42f242ac2d3de784c
+echo Win32.Trojan.Spark >> 181ee63003e5c3ec8c378030286ed7a2
+:: echo Win32.BatToExe >> b1450c25dcea0e3109ec904134795f8b
